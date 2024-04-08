26f36da202ee7013a44b71af701ac0146dabec0e
Uneeb Agha <uneeb.agha@gmail.com>
2024-04-08 16:43:06 -0400

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,7 @@
-export default function Home() {
-  const data = [
-    {
-      address: "0x1632918c383F6F700FC5E603709934D7070Ae97B",
-      ens: "abc.eth",
-      farcaster: "abc",
-    },
-    {
-      address: "0xf58c6CEbB2359263b94BB50A6b2565eE1b0ef816",
-      ens: "xyz.eth",
-      farcaster: "ZYZZ",
-    },
-    {
-      address: "0x6C562FcC1e3aF31B8bF14dDf7756492319267bA8",
-      ens: "john.eth",
-      farcaster: "Johnson Houston",
-    },
-    {
-      address: "0xde8C066fAdc67Bdf1EAdd5b8e4309de60ed8DBa7",
-      ens: "vitalik.eth",
-      farcaster: "Vitalik",
-    },
-  ];
+import prisma from "@/lib/prisma";
+
+export default async function Home(props: any) {
+  const data = await prisma.user.findMany();
   return (
     <div className="m-10">
       <table className="table-auto w-full max-w-4xl">
